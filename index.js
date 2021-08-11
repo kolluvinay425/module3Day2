975d3efc46773f677dcb126a9886325d69c1ce11
kolluvinay425 <kolluvinay425@gmail.com>
2021-08-11 13:18:25 +0200

    commit

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const callAPiData = async () => {
-  let akonMusic = await fetch("https://api.deezer.com/search?q=eminem", {
+  fetch("https://api.deezer.com/search?q=eminem", {
     headers: {
-      ContentType: "application/json; charset=utf-8",
-      Cookie:
-        "csrftoken=Nz44NWFP7wlUecHBGK9Stvm8jmVexlUniJRgyWx3KeQfu49XHp3h2dNalVvEn0zF; sessionid=4hwj4yrcuj40z355qpla4ytvusvb8jsl",
+      "x-rapidapi-key": "{x-rapidapi-key}",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
     },
-  });
-  let music = await akonMusic.json();
-  console.log(music);
+  })
+    .then((response) => response.json())
+    .then((apiData) => {
+      //displayapiData(apiData);
+      console.log(apiData);
+    })
+    .catch((err) => console.log(err));
 };
-/*.then((response) => await response.json())
-      .then((apiData) => {
-        //displayapiData(apiData);
-        console.log(apiData);
-        //console.log("img", apiData.results[0].picture);
-      })
-      .catch((err) => console.log(err));*/
-
-//callAPiData();
 
 const displayapiData = (apiData) => {
   let element = document.querySelector(".row");
@@ -26,16 +20,16 @@ const displayapiData = (apiData) => {
                   <div class="card mb-3" style="max-width: 540px">
                   <div class="row g-0">
                       <div class="col-md-4">
-                      <img src="${apiData.results[0].picture.medium}" class="img-fluid rounded-start" alt="..." />
+                      <img src="" class="img-fluid rounded-start" alt="..." />
                       </div>
                       <div class="col-md-8">
                       <div class="card-body">
-                          <h5 class="card-title">FullName: ${apiData.results[0].name.first} ${apiData.results[0].name.last}</h5>
+                          <h5 class="card-title">FullName: </h5>
                           <p class="card-text">
-                          Phone: ${apiData.results[0].cell}
+                          Phone: 
                           </p>
                           <p class="card-text">
-                          <small class="text-muted">FullName: ${apiData.results[0].name.first} ${apiData.results[0].name.last}</small>
+                          <small class="text-muted">FullName: </small>
                           </p>
                           
                       </div>
@@ -45,7 +39,16 @@ const displayapiData = (apiData) => {
                   `;
 };
 
+async function data() {
+  // data = async ()
+  let url = "https://randomuser.me/api";
+  const data = await fetch(url);
+  const dataApi = await data.json();
+  console.log("apiData: ", dataApi);
+}
 window.onload = () => {
   callAPiData();
   //displayapiData();
+
+  data();
 };
